25e11d12aaa532f5505e3e1fae995f48ccc21e26
Haoming <hmstudy02@gmail.com>
2024-06-19 17:26:54 +0800

    init

---
+++ ImageClipboard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class ImageClipboard
+{
+    [DllImport("ImageClipboard.dll", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void PasteImage(out int imageSize, out IntPtr imageData);
+
+    public static bool Paste(ref Texture2D _texture)
+    {
+        PasteImage(out int size, out IntPtr arrayPtr);
+
+        if (size < 0)
+            return false;
+
+        byte[] array = new byte[size];
+        Marshal.Copy(arrayPtr, array, 0, size);
+        Marshal.FreeHGlobal(arrayPtr);
+
+        _texture.LoadImage(array);
+        _texture.Apply();
+        return true;
+    }
+}
